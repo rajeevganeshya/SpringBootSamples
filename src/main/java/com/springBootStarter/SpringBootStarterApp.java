e8ccdb4a321697845b2b40619678d02d3b4ab2c6
rajeevganeshya <rajeevganeshya@gmail.com>
2017-10-11 14:37:23 -0400

    Adding the SpringBootSample project to Github.

---
+++ src/main/java/com/springBootStarter/SpringBootStarterApp.java
@@ -0,0 +1,14 @@
+package com.springBootStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterApp.class, args);
+		
+	}
+
+}
